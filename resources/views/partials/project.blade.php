0ad410180baa3a4fc7797bcb19a577d077c4e123
Grégory Van Ossel <vanosselgregory@gmail.com>
2021-07-04 13:53:14 +0200

    Correction flex disposition project

--- resources/views/partials/project.blade.php
+++ resources/views/partials/project.blade.php
@@ -2,20 +2,22 @@
 
     @foreach ($projects as $project)
 
-		<div class="flex flex-col mt-2">
-			<div class="flex flex-row mt-2">
+		<div class="flex flex-col mt-2 flex-grow">
+			<div class="flex-grow mt-2">
 				<div
-				class="flex w-full items-center justify-between bg-white
+				class="flex-grow w-full items-center justify-between bg-white
 				dark:bg-gray-800 px-8 py-6 border-l-4 border-indigo-700
 				dark:border-indigo-300">
 				<!-- card -->
-				<div class="flex">
-					<img
-					class="h-28 w-28 rounded object-cover"
-					src="{{ asset('project/cover/'.$project->picture) }}"
-					alt="infamous" />
+				<div class="flex justify-between">
+					<div class="flex-none">
+						<img
+						class="h-28 w-28 rounded object-cover"
+						src="{{ asset('project/cover/'.$project->picture) }}"
+						alt="infamous" />
+					</div>
 
-					<div class="flex flex-col ml-6">
+					<div class="flex-grow ml-6">
 					<span class="text-lg font-bold">{{ $project->name }}</span>
 					<p>{{ $project->about }}</p>
 					<div class="mt-4 flex">
@@ -95,103 +97,43 @@
 						</div>
 					</div>
 
-					<div class="mt-4 flex">
-						<button
-						class="flex items-center
-						focus:outline-none border rounded-full
-						py-2 px-6 leading-none border-gray-500
-						dark:border-gray-600 select-none
-						hover:bg-indigo-700 hover:text-white
-						dark-hover:text-gray-200">
-						<svg
-							class="h-5 w-5 fill-current mr-2"
-							viewBox="0 0 24 24">
-							<path
-							d="M14
-							12v7.88c.04.3-.06.62-.29.83a.996.996
-							0 01-1.41 0l-2.01-2.01a.989.989 0
-							01-.29-.83V12h-.03L4.21 4.62a1 1
-							0
-							01.17-1.4c.19-.14.4-.22.62-.22h14c.22
-							0 .43.08.62.22a1 1 0 01.17
-							1.4L14.03 12H14z"></path>
-						</svg>
-						<span>Accepter le projet</span>
-						</button>
+					<div class="mt-4 flex justify-end flex-grow">
 
                         <form method="POST" action="{{ route('favoris.store', $project->id) }}" name="frmFavorite">
                             @csrf
 
                             <input type="hidden" name="favorite" value="{{ $project->id }}" >
                             <button
-                            type = "submit"
-                            class="flex items-center ml-4
-                            focus:outline-none border rounded-full
-                            py-2 px-6 leading-none border-indigo-700
-                            dark:border-indigo-700 select-none
-                            hover:bg-indigo-700 hover:text-white
-                            dark-hover:text-gray-200">
-                            <svg
-                                class="h-5 w-5 fill-current mr-2"
-                                viewBox="0 0 576 512">
-                                <path
-                                d="M402.3 344.9l32-32c5-5
-                                13.7-1.5 13.7 5.7V464c0 26.5-21.5
-                                48-48 48H48c-26.5
-                                0-48-21.5-48-48V112c0-26.5
-                                21.5-48 48-48h273.5c7.1 0 10.7
-                                8.6 5.7 13.7l-32 32c-1.5 1.5-3.5
-                                2.3-5.7
-                                2.3H48v352h352V350.5c0-2.1.8-4.1
-                                2.3-5.6zm156.6-201.8L296.3
-                                405.7l-90.4 10c-26.2
-                                2.9-48.5-19.2-45.6-45.6l10-90.4L432.9
-                                17.1c22.9-22.9 59.9-22.9 82.7
-                                0l43.2 43.2c22.9 22.9 22.9 60 .1
-                                82.8zM460.1 174L402 115.9 216.2
-                                301.8l-7.3 65.3 65.3-7.3L460.1
-                                174zm64.8-79.7l-43.2-43.2c-4.1-4.1-10.8-4.1-14.8
-                                0L436 82l58.1 58.1
-                                30.9-30.9c4-4.2 4-10.8-.1-14.9z"></path>
-                            </svg>
-                            <span>Ajouter aux favoris</span>
+								type = "submit"
+								class="flex items-center ml-4
+								focus:outline-none border rounded-full
+								py-2 px-6 leading-none border-indigo-700
+								dark:border-indigo-700 select-none
+								hover:bg-indigo-700 hover:text-white
+								dark-hover:text-gray-200">
+								<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+									<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+								</svg>
+								<span>Ajouter aux favoris</span>
                             </button>
-
                         </form>
 
-						<button
-						class="flex items-center ml-4
-						focus:outline-none border rounded-full
-						py-2 px-6 leading-none border-indigo-700
-						dark:border-indigo-700 select-none
-						hover:bg-indigo-700 hover:text-white
-						dark-hover:text-gray-200">
-						<svg
-						class="h-5 w-5 fill-current mr-2"
-						viewBox="0 0 576 512">
-						<path
-							d="M402.3 344.9l32-32c5-5
-							13.7-1.5 13.7 5.7V464c0 26.5-21.5
-							48-48 48H48c-26.5
-							0-48-21.5-48-48V112c0-26.5
-							21.5-48 48-48h273.5c7.1 0 10.7
-							8.6 5.7 13.7l-32 32c-1.5 1.5-3.5
-							2.3-5.7
-							2.3H48v352h352V350.5c0-2.1.8-4.1
-							2.3-5.6zm156.6-201.8L296.3
-							405.7l-90.4 10c-26.2
-							2.9-48.5-19.2-45.6-45.6l10-90.4L432.9
-							17.1c22.9-22.9 59.9-22.9 82.7
-							0l43.2 43.2c22.9 22.9 22.9 60 .1
-							82.8zM460.1 174L402 115.9 216.2
-							301.8l-7.3 65.3 65.3-7.3L460.1
-							174zm64.8-79.7l-43.2-43.2c-4.1-4.1-10.8-4.1-14.8
-							0L436 82l58.1 58.1
-							30.9-30.9c4-4.2 4-10.8-.1-14.9z"></path>
-						</svg>
-						<span>Faire une offre</span>
-					</button>
-
+						<form method="GET" action="{{ route('project.show', $project->id) }}" name="frmShow">
+                            @csrf
+							<button
+								class="flex items-center ml-4
+								focus:outline-none border rounded-full
+								py-2 px-6 leading-none border-indigo-700
+								dark:border-indigo-700 select-none
+								hover:bg-indigo-700 hover:text-white
+								dark-hover:text-gray-200">
+								<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+									<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+									<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+								</svg>
+								<span>Voir le projet</span>
+							</button>
+						</form>
 					</div>
 					</div>
 				</div>
@@ -204,7 +146,7 @@
 
     <!--paginate-->
     @if(!$projects instanceof \Illuminate\Database\Eloquent\Collection)
-        <div class="flex flex-row justify-center m-7">
+        <div class="flex flex-grow justify-center m-7">
             {{ $projects->links() }}
         </div>
     @endif
