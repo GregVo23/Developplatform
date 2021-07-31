d857cc263d643608bcc760974a104abd78220945
Grégory Van Ossel <63047472+GregVo23@users.noreply.github.com>
2021-07-31 16:10:03 +0200

    Email de vérification + import vendor: email & notification + traduction email de vérification

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -8,7 +8,7 @@
 
         <div class="mb-4 mt-4 text-sm text-gray-600">
             {{ __('
-            Merci pour votre inscription! Avant de commencer, pourriez-vous vérifier votre adresse e-mail en cliquant sur le lien que nous venons de vous envoyer par e-mail ? Si vous n\'avez pas reçu l\'e-mail, nous nous ferons un plaisir de vous en envoyer un autre.') }}
+            Merci pour votre inscription ! Avant de commencer, pourriez-vous vérifier votre adresse e-mail en cliquant sur le lien que nous venons de vous envoyer par e-mail ? Si vous n\'avez pas reçu l\'e-mail, nous nous ferons un plaisir de vous en envoyer un autre.') }}
         </div>
 
         @if (session('status') == 'verification-link-sent')
